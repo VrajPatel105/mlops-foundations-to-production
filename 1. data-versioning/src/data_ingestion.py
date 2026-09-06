b06fe832ccfb28a72af3e6d0f61a208a6e106461
VrajPatel105 <vrajp1052005@gmail.com>
2026-09-06 03:51:23 -0400

    final dvc pipeline after 5 ml stages

--- 1. data-versioning/src/data_ingestion.py
+++ 1. data-versioning/src/data_ingestion.py
@@ -12,7 +12,8 @@ df.drop(columns=['tweet_id'],inplace=True)
 
 final_df = df[df['sentiment'].isin(['happiness', 'sadness'])]
 
-final_df['sentiment'].replace({'happiness':1, 'sadness':0}, inplace=True)
+final_df = final_df.copy()
+final_df["sentiment"] = final_df["sentiment"].map({"happiness": 1, "sadness": 0})
 
 train_data, test_data = train_test_split(final_df, test_size=0.2, random_state=42)
 
