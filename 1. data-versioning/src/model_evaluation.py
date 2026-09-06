31af2bc040303a547afbecef7287b6d3c3f2fd90
VrajPatel105 <vrajp1052005@gmail.com>
2026-09-06 03:50:58 -0400

    Adding Model Evaluation

---
+++ 1. data-versioning/src/model_evaluation.py
@@ -0,0 +1,40 @@
+# 5. ml pipeline : model evaluation final stage for ml pipeline
+
+import json
+import pandas as pd
+import pickle
+
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score, recall_score, roc_auc_score
+
+
+xgb = pickle.load(open("model.pkl", "rb"))
+
+
+test_data = pd.read_csv("./data/features/test_bow.csv")
+
+
+X_test = test_data.iloc[:, :-1].values
+y_test = test_data.iloc[:, -1].values
+
+
+y_pred = xgb.predict(X_test)
+y_prob = xgb.predict_proba(X_test)[:, 1]
+
+
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+auc = roc_auc_score(y_test, y_prob)
+
+
+metrics_dict = {
+    "accuracy": accuracy,
+    "precision": precision,
+    "recall": recall,
+    "roc_auc": auc
+}
+
+
+with open("metrics.json", "w") as f:
+    json.dump(metrics_dict, f, indent=4)
